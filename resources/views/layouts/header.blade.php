0619ae8a312155498ca85365bb1fc6a3ef017c81
n-moghaddasi-ir <nmgh.developer@gmail.com>
2022-12-17 00:02:52 +0330

    run first view of main page:
    add layout directory containing header and footer in blade,
    add main page content in view,
    create rout for load index page

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title> @yield('title') </title>
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset('css/style-starter.css')}}">
+  <!-- Template CSS -->
+  <link href="{{asset('fonts/font-face.css')}}" rel="stylesheet">
+  <!-- Template CSS -->
+
+</head>
+<body>
+
+    @yield('content')
+    @yield('footer')
---
+++ resources/views/main.blade.php
@@ -0,0 +1,389 @@
+@extends('layouts.header')
+@extends('layouts.footer')
+
+@section('title', 'Narges Moghaddasi')
+
+@section('content')
+  <!-- /Homepage-->
+  <section class="w3l-banner-main">
+    <!-- //Header -->
+    <section class="banner-main-inf" id="section07">
+      <header id="site-header" class="header-top">
+        <nav class="navbar navbar-expand-lg shift " id="header">
+          <a class="navbar-brand" href="index.html">
+            Narges<span class="dot-1">.</span>
+          </a>
+          <button class="navbar-toggler bg-gradient" type="button" data-toggle="collapse"
+            data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+            aria-label="Toggle navigation"><span class="navbar-toggler-icon fa fa-bars"> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#site-header">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link effect-3" href="about.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link effect-3" href="services.html">Services</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link effect-3" href="contact.html">Contact</a>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
+      </header>
+      <!-- //Header -->
+      <!--/banner-content-->
+      <div class="bannerhny-content text-center">
+        <div class="layer">
+          <div class="bannerhnyinf">
+            <h6>Hello !!!</h6>
+            <h3>I'm Narges Moghaddasi</h3>
+            <h4 class="ban-t"> I’m <span class="typed-text"></span><span class="cursor">&nbsp</span></h4>
+            <div class="scroll-button">
+              <a href="#about" class="scroll">
+                <div class="icon-scroll">
+                  <div class="mouse">
+                    <div class="wheel"></div>
+                  </div>
+                  <div class="icon-arrows">
+                    <span></span>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!--//banner-content-->
+      </div>
+      </div>
+    </section>
+    <!-- //Homepage -->
+<section class="w3l-content-w-photo-6" id="about">
+	<!-- /content-grids-->
+	<div class="content-photo-info py-5">
+		<div class="container py-lg-3">
+			<!-- /row-->
+			<div class="content-photo-grids row mt-lg-5 mt-4">
+				<div class="photo-6-inf-left col-lg-6 pr-lg-4">
+					<img src="assets/images/ab.jpg" class="img-fluid" alt="">
+				</div>
+				<div class="photo-6-inf-right col-lg-6 text-left pl-lg-5">
+
+					<h3 class="hny-title">
+						About Me<span class="dot-1">.</span></h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur hic odio voluptatem tenetur
+						consequatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur hic odio
+						voluptatem tenetur
+						consequatur adipisicing elit.</p>
+					<div class="read-more mt-lg-5 mt-4">
+						<a href="about.html" class="read-more">
+							<span class="more_icon fa fa-long-arrow-right"></span>
+							<span class="more_text">Read More</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			<!-- //row-->
+			<!-- /row-->
+			<div class="about-content-grids row py-lg-5 py-4 mt-lg-5 mt-4">
+				<div class="col-lg-4 col-md-6 about-gd mt-3">
+					<div class="about-gd-inn">
+						<span class="fa fa-magic icon-fea1" aria-hidden="true"></span>
+						<h5><a href="#URL">Modern design</a></h5>
+						<p>Lorem ipsum dolor sit amet,Ea consequuntur illum facere.</p>
+
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 about-gd mt-3">
+					<div class="about-gd-inn">
+						<span class="fa fa-umbrella icon-fea1" aria-hidden="true"></span>
+						<h5><a href="#URL">User Experience</a></h5>
+						<p>Lorem ipsum dolor sit amet,Ea consequuntur illum facere .</p>
+
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 about-gd mt-3">
+					<div class="about-gd-inn">
+						<span class="fa fa-pencil-square-o icon-fea1" aria-hidden="true"></span>
+						<h5><a href="#URL">Powerful Editor</a></h5>
+						<p>Lorem ipsum dolor sit amet,Ea consequuntur illum facere.</p>
+
+					</div>
+				</div>
+
+			</div>
+			<!-- //row-->
+
+		</div>
+	</div>
+</section>
+<!-- //content-grids-->
+
+<section class="w3l-content-6">
+	<!-- /content-6-section -->
+	  <div class="content-6-mian py-5">
+			 <div class="container py-lg-3">
+					 <div class="content-info-in row">
+							 <div class="content-gd col-lg-5">
+								<h3 class="hny-title">Motivated by the desire to <br>achieve<span class="dot-1">.</span>
+							 </div>
+							 <div class="content-gd col-lg-7">
+								<div class="progress-info">
+									<h6 class="progress-tittle">UI/UX</h6>
+									<div class="progress">
+										<div class="progress-bar progress-bar-striped" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+										</div>
+									</div>
+								</div>
+								<div class="progress-info">
+									<h6 class="progress-tittle">Ideas
+									</h6>
+									<div class="progress">
+										<div class="progress-bar progress-bar-striped" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
+										</div>
+									</div>
+								</div>
+								<div class="progress-info">
+									<h6 class="progress-tittle">Marketing</h6>
+									<div class="progress">
+										<div class="progress-bar progress-bar-striped" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
+										</div>
+									</div>
+								</div>
+							 </div>
+					 </div>
+				 </div>
+			 </div>
+	 </section>
+   <!-- //content-6-section -->
+
+
+<section class="w3-gallery">
+	<!-- Portfolio -->
+	<div class="portfolio py-5">
+		<div class="container py-lg-5">
+
+			<h3 class="hny-title">Stunning Projects<span class="dot-1">.</span></h3>
+
+			<!-- /Portfolio-grids -->
+			<ul class="portfolio-categ filter my-md-5 my-4 p-0">
+				<li class="port-filter all active">
+					<a href="#">All</a>
+				</li>
+				<li class="cat-item-1">
+					<a href="#" title="Category 1">Photography</a>
+				</li>
+				<li class="cat-item-2">
+					<a href="#" title="Category 2">Web Design</a>
+				</li>
+				<li class="cat-item-3">
+					<a href="#" title="Category 3">Creative</a>
+				</li>
+
+			</ul>
+			<ul class="portfolio-area clearfix p-0 m-0 row">
+				<li class="portfolio-item2 content" data-id="id-1" data-type="cat-item-1">
+					<span class="image-block">
+
+						<a class="image-zoom" href="assets/images/g1.jpg" data-gal="prettyPhoto[gallery]">
+							<div class="content-overlay"></div>
+							<img src="assets/images/g1.jpg" class="img-fluid" alt="portfolio-img">
+
+							<div class="content-details fadeIn-bottom">
+								<h3 class="content-title">Add Your Title</h3>
+
+							</div>
+						</a>
+					</span>
+				</li>
+				<li class="portfolio-item2 content" data-id="id-2" data-type="cat-item-2">
+					<span class="image-block">
+
+						<a class="image-zoom" href="assets/images/g2.jpg" data-gal="prettyPhoto[gallery]">
+							<div class="content-overlay"></div>
+							<img src="assets/images/g2.jpg" class="img-fluid" alt="portfolio-img">
+							<div class="content-details fadeIn-bottom">
+								<h3 class="content-title">Add Your Title</h3>
+
+							</div>
+						</a>
+					</span>
+				</li>
+				<li class="portfolio-item2 content" data-id="id-3" data-type="cat-item-3">
+					<span class="image-block">
+
+						<a class="image-zoom" href="assets/images/g3.jpg" data-gal="prettyPhoto[gallery]">
+							<div class="content-overlay"></div>
+							<img src="assets/images/g3.jpg" class="img-fluid" alt="portfolio-img">
+							<div class="content-details fadeIn-bottom">
+								<h3 class="content-title">Add Your Title</h3>
+
+							</div>
+						</a>
+					</span>
+				</li>
+				<li class="portfolio-item2 content" data-id="id-4" data-type="cat-item-2">
+					<span class="image-block">
+
+						<a class="image-zoom" href="assets/images/g4.jpg" data-gal="prettyPhoto[gallery]">
+							<div class="content-overlay"></div>
+							<img src="assets/images/g4.jpg" class="img-fluid" alt="portfolio-img">
+							<div class="content-details fadeIn-bottom">
+								<h3 class="content-title">Add Your Title</h3>
+
+							</div>
+						</a>
+					</span>
+				</li>
+
+
+			</ul>
+			<!--end portfolio-area -->
+			<!-- /Portfolio-grids -->
+		</div>
+
+	</div>
+</section>
+<!-- //Portfolio -->
+<!-- blog with slider -->
+<section class="blog-slider">
+    <div class="blog-slider-inner py-5">
+        <div class="container py-lg-5">
+            <h3 class="hny-title two">
+                Blogs & News<span class="dot-1">.</span></h3>
+            <div class="row inner-sec-w3layouts mt-4">
+                <div class="col-lg-4 col-md-6 content-left-sec blog-slide-grid mt-4">
+                    <div class="blog-slide-img">
+                        <a href="#"><img src="assets/images/1.jpg" class="img img-fluid" alt=""></a>
+                    </div>
+                    <div class="blog-slide-info">
+
+                        <a href="#">
+                            <h4 class="mt-4 mb-0">Blog post title 1</h4>
+                        </a>
+                        <h6 class="mt-sm-2 mt-1">Dec 6th, 2020</h6>
+
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 content-left-sec blog-slide-grid mt-4">
+                    <div class="blog-slide-img">
+                        <a href="#"><img src="assets/images/2.jpg" class="img img-fluid" alt=""></a>
+                    </div>
+                    <div class="blog-slide-info">
+
+                        <a href="#">
+                            <h4 class="mt-4 mb-0">Blog post title 2</h4>
+                        </a>
+                        <h6 class="mt-sm-2 mt-1">Dec 6th, 2020</h6>
+
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 content-left-sec blog-slide-grid mt-4">
+                    <div class="blog-slide-img">
+                        <a href="#"><img src="assets/images/3.jpg" class="img img-fluid" alt=""></a>
+                    </div>
+                    <div class="blog-slide-info">
+
+                        <a href="#">
+                            <h4 class="mt-4 mb-0">Blog post title 3</h4>
+                        </a>
+                        <h6 class="mt-sm-2 mt-1">Dec 6th, 2020</h6>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- //blog with slider -->
+<section class="w3l-customers-6">
+	<!--/customers -->
+	<div class="customers-6-infhny py-5">
+		<div class="container py-lg-5">
+			<!--/customers -->
+			<div id="owl-demo2" class="owl-carousel owl-theme">
+				<!--/slide1-->
+				<div class="item">
+					<div class="teastimonials-content">
+						<div class="teastimonials-left">
+								<div class="teasti-innf">
+										<p class="overview"><b>Antonio Moreno</b><span class="city">Company Exp.</span></p>
+										<p class="testimonial">Lorem ipsum dolor sit amet,Ea consequuntur illum facere aperiam sequi optio consectetur.Vivamus a ligula quam. Ut blandit eu leo non suscipit. Duis feugiat tortor sed.Nulla quis lorem neque, mattis venenatis lectus.</p>
+								</div>
+						</div>
+						<div class="teastimonials-right">
+								<img
+								src="assets/images/team2.jpg" class="img-fluid" alt="">
+						</div>
+					</div>
+				</div>
+				<!--//slide1-->
+				<!--/slide1-->
+				<div class="item">
+					<div class="teastimonials-content">
+						<div class="teastimonials-left">
+								<div class="teasti-innf">
+										<p class="overview"><b>Daniel Smith </b><span class="city">Company Exp.</span></p>
+										<p class="testimonial">Lorem ipsum dolor sit amet,Ea consequuntur illum facere aperiam sequi optio consectetur.Vivamus a ligula quam. Ut blandit eu leo non suscipit. Duis feugiat tortor sed.Nulla quis lorem neque, mattis venenatis lectus.</p>
+								</div>
+						</div>
+						<div class="teastimonials-right">
+								<img
+								src="assets/images/team3.jpg" class="img-fluid" alt="">
+						</div>
+					</div>
+				</div>
+				<!--//slide1-->
+				<!--/slide1-->
+				<div class="item">
+					<div class="teastimonials-content">
+						<div class="teastimonials-left">
+							<div class="teasti-innf">
+									<p class="overview"><b>Antonio Daniel </b><span class="city">Company Exp.</span></p>
+									<p class="testimonial">Lorem ipsum dolor sit amet,Ea consequuntur illum facere aperiam sequi optio consectetur.Vivamus a ligula quam. Ut blandit eu leo non suscipit. Duis feugiat tortor sed.Nulla quis lorem neque, mattis venenatis lectus.</p>
+							</div>
+						</div>
+						<div class="teastimonials-right">
+								<img src="assets/images/team1.jpg" class="img-fluid" alt="">
+
+						</div>
+					</div>
+				</div>
+				<!--//slide1-->
+
+			</div>
+			<!--//customers -->
+		</div>
+</section>
+<!-- //customers-6-->
+    <!--/w3l-download-main-->
+    <section class="w3l-download-main">
+    	<!--/download-->
+    	<div class="download-content text-center">
+    		<div class="layer-2 py-5">
+    			<div class="container py-lg-3">
+    				<h3 class="hny-title two">Do you like my work<span class="dot-1">.</span>?</h3>
+    				<h6>Let’s work together!</h6>
+    				<div class="read-more mt-lg-5 mt-4">
+    					<a href="contact.html" class="read-more">
+    						<span class="more_icon fa fa-long-arrow-right"></span>
+    						<span class="more_text">Get In Touch</span>
+    					</a>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	</div>
+    	<!--//download-->
+    </section>
+    <!--//w3l-download-main-->
+@endsection()
